6d606102d4488f9bd945e9d0c675ad7a46a70fbe
poseidon0z <adityaprabhu1290@gmail.com>
2023-03-06 16:40:48 +0530

    Created a tile object

--- Slide/src/Slide.java
+++ Slide/src/Slide.java
@@ -2,7 +2,19 @@
 public class Slide {
 
 	public static void main(String[] args) {
-		System.out.println("Check this out...");
+		
+		Tile tile1 = new Tile();
+		tile1.setID(0);
+		tile1.setX(0);
+		tile1.setY(1);
+		
+		Tile tile2 = new Tile();
+		tile2.setID(1);
+		tile2.setX(0);
+		tile2.setY(0);
+		
+		System.out.println("Tile 1 has ID: " + tile1.getID() + " and is at position (" + tile1.getX() + ", " + tile1.getY() + ")");
+		System.out.println(String.format("Tile 2 has ID: %d and is at position (%d, %d)", tile2.getID(), tile2.getX(), tile2.getY()));
 	}
 
 }
---
+++ Slide/src/Tile.java
@@ -0,0 +1,34 @@
+
+public class Tile {
+	
+	private int tileID;
+	private int tileX;
+	private int tileY;
+	
+	//getters
+	public int getID() {
+		return tileID;
+	}
+	public int getX() {
+		return tileX;
+	}
+	public int getY() {
+		return tileY;
+	}
+	
+	//setters
+	public void setID(int ID) {
+		tileID = ID;
+	}
+	public void setX(int x) {
+		tileX = x;
+	}
+	public void setY(int y) {
+		tileY = y;
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
