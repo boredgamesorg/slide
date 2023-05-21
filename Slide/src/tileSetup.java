e619ddd3436732e9e60f41704a99c327dabf6b09
poseidon0z <adityaprabhu1290@gmail.com>
2023-05-21 16:32:03 +0530

    Made a class for setting up the board

---
+++ Slide/src/tileSetup.java
@@ -0,0 +1,39 @@
+import javax.swing.JButton;
+
+public class tileSetup {
+	static int dimension = 3;
+	static int emptyTileIndex = 8;
+	
+	public static void changeDimension(int newDimension) {
+		changeDimension(newDimension, true);
+	}
+	
+	public static void changeDimension(int newDimension, boolean changeEmptyTileIndex) {
+		dimension = newDimension;
+		if (changeEmptyTileIndex == true) {
+			emptyTileIndex = newDimension * newDimension - 1;
+		}
+	}
+	
+	public static void changeEmptyTile(int newIndex) {
+		emptyTileIndex = newIndex;
+	}
+	
+	public static Tile[] setup(JButton[] buttons) {
+		int len = dimension * dimension;
+		Tile board[] = new Tile[len];
+		
+		if (buttons.length != len) {
+			throw new IllegalArgumentException("The number of buttons is not correct for dimension provided! (" +
+		buttons.length + " provided " + len + " needed)");
+		}
+		
+		for (int i = 0; i < len; i++) {
+			board[i] = new Tile(i, buttons[i]);
+		}
+		
+		board[emptyTileIndex].setEmpty(true);
+		return board;
+	}
+	
+}
